40e7e9909ae01028cfa78f847ee5c0a13288f98a
idevol <saul.diaz.v@gmail.com>
2018-09-18 13:52:53 -0500

    BtqNPS

---
+++ plugins/BtqNPS/BtqNPS.php
@@ -0,0 +1,8 @@
+<?php
+    class BtqNPS extends \LimeSurvey\PluginManager\PluginBase {
+
+        protected $storage = 'DbStorage';
+        static protected $description = 'BTQ Design NPS Plugin';
+        static protected $name = 'btqnps';
+    }
+}
