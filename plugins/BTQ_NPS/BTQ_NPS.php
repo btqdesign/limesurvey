6efbfb33bc64760214778f654dfa7d825f6baf97
idevol <saul.diaz.v@gmail.com>
2018-09-19 17:31:34 -0500

    Plugin

--- plugins/BTQ_NPS/BTQ_NPS.php
+++ plugins/BTQ_NPS/BTQ_NPS.php
@@ -10,7 +10,7 @@ class BTQ_NPS extends \LimeSurvey\PluginManager\PluginBase
         $this->subscribe('beforeSurveyPage');
         $this->subscribe('afterSavedResponse');
         $this->subscribe('beforeActivate');
-        $this->subscribe('beforeAdminMenuRender');
+        $this->subscribe('onPluginRegistration');
     }
 
     public function beforeSurveyPage()
@@ -53,31 +53,36 @@ class BTQ_NPS extends \LimeSurvey\PluginManager\PluginBase
         }
     }
 
-    public function beforeToolsMenuRender()
-    {
-        $event = $this->getEvent();
-        $surveyId = $event->get('surveyId');
-        $href = Yii::app()->createUrl(
-            'admin/pluginhelper',  // Call the plugin helper controller
-            array(
-                'sa' => 'sidebody',  // subaction is "sidebody"
-                'plugin' => 'BTQ_NPS',  // Name of this plugin
-                'method' => 'actionIndex',  // Plugin action that PluginHelper will call
-                'surveyId' => $surveyId
-            )
+    public function onPluginRegistration(){
+        $pluginName = $this->get('btq_nps');
+        //Maybe add a check for the correct pluginName here
+        //Check if the menu/menu entries are already created use yii database methods for that
+        
+        $menuArray = array(
+            "parent_id" => 1, //1 -> main surveymenu, 2-> quickemenu, NULL -> new base menu in sidebar
+            "name" => "btq_nps",
+            "title" => "BTQ NPS Title",
+            "position" => "side", // possible positions are "side" and "collapsed" state 3.0.0.beta-2
+            "description" => "Descripcion"
         );
-        // TODO: Class ExtraMenuItem is not part of core yet
-        $menuItem = new ExtraMenuItem(array(
-            'label' => 'BTQ NPS',
-            'iconClass' => 'fa fa-table',
-            'href' => $href
-        ));
-        $event->set('menuItems', array($menuItem));
-    }
+        $newMenuId = Surveymenu::staticAddMenu($menuArray);
     
-    public function actionIndex()
-    {
-        // TODO: Add view and layout
-        return "This is the content of the side-body";
+        // repeat this as often as you need it
+        $menuEntryArray = array(
+            "name" => "accion",
+            "title" => "Titulo de la accion",
+            "menu_title" => "Titulo del menu", 
+            "menu_description" => "Descripcion de la accion",
+            "menu_icon" => "empire", //it is either the fontawesome classname withot the fa- prefix, or the iconclass classname, or a src link
+            "menu_icon_type" => "fontawesome", // either 'fontawesome', 'iconclass' or 'image'
+            "menu_link" => "[admin/]controller/sa/action", //the link will be parsed through yii's createURL method
+            "addSurveyId" => true, //add the surveyid parameter to the url
+            "addQuestionGroupId" => true, //add gid parameter to url
+            "addQuestionId" => true, //add qid parameter to url
+            "linkExternal" => false, //open link in a new tab/window
+            "hideOnSurveyState" => null, //possible values are "active", "inactive" and null
+            "manualParams" => "" //please read up on this setting as it may render the link useless
+        );
+        SurveymenuEntries::staticAddMenuEntry($newMenuId, $menuEntryArray);
     }
 }
