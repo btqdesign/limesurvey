afa652e6550f58783dbf31c2b6e9700ae60670e7
Jorge Santana <jorge@btqdesign.com>
2018-10-23 15:25:26 -0500

    cambiar vista

--- themes/question/caritas/survey/questions/answer/arrays/10point/rows/cells/answer_td_input.php
+++ themes/question/caritas/survey/questions/answer/arrays/10point/rows/cells/answer_td_input.php
@@ -10,8 +10,9 @@
 ?>
 
 <!-- answer_td_input -->
-<td class="answer_cell_<?php echo $i;?><?php echo ($i==="") ? ' noanswer-item':''; ?> answer-item radio-item">
-    <input
+<tr class="answer_cell_<?php echo $i;?><?php echo ($i==="") ? ' noanswer-item':''; ?> answer-item radio-item">
+   <td>
+   <input
         type="radio"
         name="<?php echo $myfname; ?>"
         id="answer<?php echo $myfname; ?>-<?php echo $i;?>"
@@ -21,5 +22,6 @@
     <label for="answer<?php echo $myfname;?>-<?php echo $i; ?>"  class="ls-label-xs-visibility">
         <?php echo $labelText;?>
     </label>
-</td>
+   </td>
+</tr>
 <!-- end of answer_td_input -->
