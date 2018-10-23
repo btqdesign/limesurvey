3d8b049f0f0bb38e6b35d2a0e78a022c508a9d63
Jorge Santana <jorge@btqdesign.com>
2018-10-23 14:19:40 -0500

    nueva vista para nps

--- themes/question/nps/survey/questions/answer/arrays/10point/answer.php
+++ themes/question/nps/survey/questions/answer/arrays/10point/answer.php
@@ -1,66 +1,40 @@
 <?php
 /**
- * Ranking question, item list header Html
- * @var $sOptions         : the select options, generated with the view answer_row.php
+ * array 10 point choice Html
  *
- * @var $name
- * @var $myfname
- * @var $labeltext
- * @var $rankId
- * @var $rankingName
- * @var $max_answers
- * @var $min_answers
- * @var $qid
- * @var $choice_title
- * @var $rank_title
- * @var $rank_help
- * @var $showpopups
- * @var $samechoiceheight
- * @var $samelistheight
+ * @var $sColumns   : the columns, generated with the view columns/col.php
+ * @var $sHeaders   : the headers, generated with the view rows/cell/thead.php
+ * @var $sRows      : the rows, generated with the view rows/answer_row.php
+ * @var $extraclass
+ * @var $answerwidth
  */
 ?>
-
-<!-- Ranking -->
+<!-- Array 10 point choice -->
 
 <!-- answer -->
-
-sin debug
-<div class="<?php echo $coreClass; ?>">
-    <ul class="list-unstyled ls-js-hidden-sr answers-list select-list " role="group" aria-labelledby="ls-question-text-<?php echo $basename; ?>">
+<table class="<?php echo $coreClass; ?> table table-bordered table-hover table-10-point-array" role="group" aria-labelledby="ls-question-text-<?php echo $basename ?>">
+    <!-- Columns -->
+    <colgroup class="col-responses">
+        <col class="col-answers" style='width: <?php echo $answerwidth; ?>%;' />
+        <?php
+            // columns/col.php
+            echo $sColumns;
+        ?>
+    </colgroup>
+    <thead aria-hidden="true">
+        <tr class="ls-heading">
+            <?php
+                // rows/cell/thead.php
+                echo $sHeaders;
+            ?>
+        </tr>
+    </thead>
+    <tbody>
         <?php
             // rows/answer_row.php
-            echo  $sSelects;
+            echo $sRows;
         ?>
-    </ul>
-    <div class="ls-no-js-hidden answers-list<?php echo ($samechoiceheight) ? " list-samechoiceheight": "" ?><?php echo ($samelistheight) ? " list-samelistheight": "" ?> row" aria-hidden="true">
-        <div class="col-sm-6 col-xs-6">
-            <strong class="sortable-subtitle sortable-rank-subtitle"><?php echo $rank_title;?></strong>
-            <!-- @todo : move htmlblock at the good place -->
-            <ul id="sortable-choice-<?php echo $qId;?>" class="sortable-choice sortable-list list-group">
-                <?php foreach ($answers as $ansrow):?>
-                    <li id="javatbd<?php echo $rankingName; ?><?php echo $ansrow['code'];?>" class="ls-choice list-group-item answer-item sortable-item sortable-enable" data-value="<?php echo $ansrow['code'];?>">
-                        <?php echo $ansrow['answer']; ?>
-                    </li>
-                <?php endforeach;?>
-                <li class="hidden ls-remove"></li>
-            </ul>
-        </div>
-      
-    </div>
-</div>
+    </tbody>
+</table>
 
-<!--  The list with HTML answers -->
-
-<div class="hidden" style="display:none">
-    <div style='display:none' id='ranking-<?php echo $qId;?>-maxans'>
-        <?php echo $max_answers;?>
-    </div>
-    <div style='display:none' id='ranking-<?php echo $qId;?>-minans'>
-        <?php echo $min_answers;?>
-    </div>
-    <div style='display:none' id='ranking-<?php echo $qId;?>-name'>
-        <?php echo $rankingName;?>
-    </div>
-
-</div>
 <!-- end of answer -->
